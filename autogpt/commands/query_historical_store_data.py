fb61ac0151d52cee127738c5bc8433caf602ed6f
黄茵 <huangyin@huangyindeMacBook-Pro.local>
2023-05-22 14:03:01 +0800

    fix prompt

--- autogpt/commands/query_historical_store_data.py
+++ autogpt/commands/query_historical_store_data.py
@@ -3,23 +3,22 @@ from __future__ import annotations
 
 from autogpt.commands.command import command
 
-MOCK_DATE = {
-    "20230518":''' 
-Operating hours :40 
-Peak hours :76 
-Minimum delivery score :80 
-Store richness score :32 
-Service richness score :80 
-Marketing activity richness score :77 
-The manual response rate of the recent 7 days of poor evaluation :32 
-IM online response rate :89 
-Store score :70 
-Quality commodity rate :80 
-Menu richness score :68 
-Business responsibility cancellation rate score :70 
+MOCK_DATE = """
+each factor score of the store with id=223524545 :
+Operating hours score:40 \n
+Peak hours score:76 \n
+Minimum delivery score :80 \n
+Store richness score :32 \n
+Service richness score :80 \n
+Marketing activity richness score :77 \n
+The manual response rate of the recent 7 days of poor evaluation :32 \n
+IM online response rate :89 \n
+Store score :70 \n
+Quality commodity rate :80 \n
+Menu richness score :68 \n
+Business responsibility cancellation rate score :70 \n
 Meal reporting rate :80 
-'''
-}
+"""
 
 @command(
     "query_historical_store_data",
