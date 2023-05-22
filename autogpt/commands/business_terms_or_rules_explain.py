2a7e19b4125039003810f271619ef99a49226c4a
黄茵 <huangyin@huangyindeMacBook-Pro.local>
2023-05-22 14:08:52 +0800

    fix prompt

--- autogpt/commands/business_terms_or_rules_explain.py
+++ autogpt/commands/business_terms_or_rules_explain.py
@@ -4,20 +4,20 @@ from __future__ import annotations
 from autogpt.commands.command import command
 
 MOCK = """
-Store quality scoring rules:
-1,Operating hours score, weight:5%, caliber:Average operating hours of the last 7 days
-2,Peak operating hours score, weight:15%, caliber:Average peak operating hours of recent 7 days
-3,Minimum delivery price score, weight:2%, caliber:Lowest starting price yesterday
-4,Store richness score, weight:9%, caliber:Number of types of stores installed yesterday
-5,Service richness score, weight:5%, caliber:Yesterday's service richness
-6,Marketing activity richness score, weight:10%, caliber:Yesterday marketing richness
-7,Manual response rate of negative comments in recent 7 days score, weight:4%, caliber:Manual response rate of negative comments in recent 7 days
-8,IM online response rate score, weight:10%, caliber:Online response rate within 5 minutes in recent 7 days
-9,Store rating score, weight:4%, caliber:Yesterday's business rating
-10,Quality commodity rate score, weight:10%, caliber:Yesterday's rate of quality goods = number of quality goods/number of online goods
-11,Menu richness score, weight:10%, caliber:Richness of yesterday's menu
-12,Business responsibility cancellation rate score, weight:5%, caliber:Cancellation rate of business responsibility in recent 7 days
-13,Reporting rate of meals served score, weight:11%, caliber:Meal reporting rate of the last 7 days/overtime demand rate of the last 7 days
+Store quality scoring rules:\\n
+1,Operating hours score, weight:5%, caliber:Average operating hours of the last 7 days\\n
+2,Peak operating hours score, weight:15%, caliber:Average peak operating hours of recent 7 days\\n
+3,Minimum delivery price score, weight:2%, caliber:Lowest starting price yesterday\\n
+4,Store richness score, weight:9%, caliber:Number of types of stores installed yesterday\\n
+5,Service richness score, weight:5%, caliber:Yesterday's service richness\\n
+6,Marketing activity richness score, weight:10%, caliber:Yesterday marketing richness\\n
+7,Manual response rate of negative comments in recent 7 days score, weight:4%, caliber:Manual response rate of negative comments in recent 7 days\\n
+8,IM online response rate score, weight:10%, caliber:Online response rate within 5 minutes in recent 7 days\\n
+9,Store rating score, weight:4%, caliber:Yesterday's business rating\\n
+10,Quality commodity rate score, weight:10%, caliber:Yesterday's rate of quality goods = number of quality goods/number of online goods\\n
+11,Menu richness score, weight:10%, caliber:Richness of yesterday's menu\\n
+12,Business responsibility cancellation rate score, weight:5%, caliber:Cancellation rate of business responsibility in recent 7 days\\n
+13,Reporting rate of meals served score, weight:11%, caliber:Meal reporting rate of the last 7 days/overtime demand rate of the last 7 days\\n
 """
 
 @command(
--- autogpt/commands/query_historical_store_data.py
+++ autogpt/commands/query_historical_store_data.py
@@ -5,18 +5,18 @@ from autogpt.commands.command import command
 
 MOCK_DATE = """
 each factor score of the store with id=223524545 :
-Operating hours score:40 \n
-Peak hours score:76 \n
-Minimum delivery score :80 \n
-Store richness score :32 \n
-Service richness score :80 \n
-Marketing activity richness score :77 \n
-The manual response rate of the recent 7 days of poor evaluation :32 \n
-IM online response rate :89 \n
-Store score :70 \n
-Quality commodity rate :80 \n
-Menu richness score :68 \n
-Business responsibility cancellation rate score :70 \n
+Operating hours score:40 \\n
+Peak hours score:76 \\n
+Minimum delivery score :80 \\n
+Store richness score :32 \\n
+Service richness score :80 \\n
+Marketing activity richness score :77 \\n
+The manual response rate of the recent 7 days of poor evaluation :32 \\n
+IM online response rate :89 \\n
+Store score :70 \\n
+Quality commodity rate :80 \\n
+Menu richness score :68 \\n
+Business responsibility cancellation rate score :70 \\n
 Meal reporting rate :80 
 """
 
